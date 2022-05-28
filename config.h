1735d75e4d73365cf1543593c3d69f5456ab7102
Joinemm <joonas@rautiola.co>
2022-05-28 15:26:19 +0300

    fix update signals

--- config.h
+++ config.h
@@ -3,23 +3,23 @@
 #ifdef LAPTOP_BAR
 static const Block blocks[] = {
 	/*Icon	/Command				/Update Interval	/Update Signal */
-    {" ",	"echo \"$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)\"", 1,  0},
-	{" ",	"status-cpu",                                                   1,  0},
+    {" ",	"echo \"$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)\"", 1,  1},
+	{" ",	"status-cpu",                                                   1,  2},
 	{"",	"status-volume",		                                        60, 10},
 	{" ",	"light | xargs printf '%.0f\n'",                                60,	11},
-	{" ",	"acpi -b | cut -f 2 -d ',' | sed -r 's/( )+//g'",               10, 0},
+	{" ",	"acpi -b | cut -f 2 -d ',' | sed -r 's/( )+//g'",               10, 5},
 	{"",	"status-bt",                                                    60, 12},
 	{"",	"status-wifi",                                                  5,  4},
-	{" ",	"printf \"%s\" \"$(date '+%b %d (%a) %H:%M:%S') \"",            1,  0},
+	{" ",	"printf \"%s\" \"$(date '+%b %d (%a) %H:%M:%S') \"",            1,  3},
 };
 #endif
 
 #ifdef DESKTOP_BAR
 static const Block blocks[] = {
 	/*Icon	/Command				/Update Interval	/Update Signal */
-    {" ",	"echo \"$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)\"", 1,  0},
-	{" ",	"status-cpu",                                                   1,  0},
-	{" ",	"printf \"%s\" \"$(date '+%b %d (%a) %H:%M:%S') \"",            1,  0},
+    {" ",	"echo \"$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)\"", 1,  1},
+	{" ",	"status-cpu",                                                   1,  2},
+	{" ",	"printf \"%s\" \"$(date '+%b %d (%a) %H:%M:%S') \"",            1,  3},
 };
 #endif
 
