53f1ae760d48bdfc96cc88d3d6e640058b4de5ed
Joinemm <joonas@rautiola.co>
2022-10-24 19:30:33 +0300

    Add clickable blocks

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static const Block blocks[] = {
 	/*Icon	/Command				            /Interval   /Update Signal */
 	{" ",	"free -h | awk '/^Mem/ {print $3}'",1,          1},
-	{" ",	"status-cpu",                       1,          1},
+	{" ",	"status-cpu",                       1,          2},
 	{"",	"status-volume",                    30,         10},
 	{" ",	"light -r",                         30,         11},
-    {"",	"status-battery",                   1,          1},
+    {"",	"status-battery",                   1,          3},
 	{"",	"status-bt",                        30,         12},
 	{"",	"status-wifi",                      1,          14},
 	{"",	"status-layout",                    1,          13},
-	{" ",	"date '+%b %d (%a) %H:%M:%S'",      1,          1},
+	{" ",	"status-date",						1,          4},
 };
 #endif
 
@@ -19,10 +19,11 @@ static const Block blocks[] = {
 static const Block blocks[] = {
 	/*Icon	/Command				            /Interval	/Update Signal */
     {" ",  "status-mic-volume",                1,          1},
-    {" ",	"free -h | awk '/^Mem/ {print $3}'",1,          1},
-	{" ",	"status-cpu",                       1,          1},
+	{"",	"status-volume",                    30,         10},
+    {" ",	"free -h | awk '/^Mem/ {print $3}'",1,          2},
+	{" ",	"status-cpu",                       1,          3},
 	{"",	"status-layout",                    1,          13},
-	{" ",	"date '+%b %d (%a) %H:%M:%S'",      1,          1},
+	{" ",	"status-date",						1,          4},
 };
 #endif
 
